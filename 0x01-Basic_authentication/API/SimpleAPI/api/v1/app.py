515de81202f80aa17c650567eb864a5e15ebbffa
Timmygee21 <saytimmy@gmail.com>
2023-09-08 13:10:36 +0100

    file

--- 0x01-Basic_authentication/API/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/API/SimpleAPI/api/v1/app.py
@@ -4,7 +4,6 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.views import app
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
